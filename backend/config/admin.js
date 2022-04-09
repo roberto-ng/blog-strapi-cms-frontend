8d6d9f68f151f0c2dfca62a46fd039800cd15c2f
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-04-09 16:12:27 -0300

    Mudar front-end de repo

--- backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '9dde1ba7c8270d8c035b507c913fc183'),
-  },
-});
--- backend/config/env/production/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("HEROKU_URL"),
-    app: {
-        keys: env.array('APP_KEYS'),
-    },
-});
